c0b07c33910d2ee8caa49e8b7852c7e05ca65011
Jingyi3 <oliz0991@gmail.com>
2020-01-29 13:20:31 -0500

    Initial commit python code of beginner

---
+++ pylearn/app.py
@@ -0,0 +1,62 @@
+# print("Hello! Olivia!")
+#
+# print('o----')
+# print(' ||||')
+# print('*' * 10) # expression : a piece of code that produce a value
+# #interpreter execute linebyline
+#
+# price = 10
+# # convert to binary and store in
+# rating = 4.9
+# label = 'Olivia'
+# is_published = False
+# #variable should be lowercase, simple value up
+# print(price)
+#
+# full_name = 'John Smith'
+# age = 20
+# is_exist = False
+#
+# # receive input
+# # () calling the function
+# name = input('what is your name? ')
+# color = input('what is your favorite color? ')
+# print(name + ' likes ' + color)
+
+
+# # type conversion
+# # int() function
+# birth_year = input('Birth year: ')
+# print(type(birth_year)) # <class 'str'>
+# age = 2020 - int(birth_year) # <class 'int'>
+# print(type(age))
+# print(age)
+
+# weight_pounds = input("what is your weight? ")
+# weight_kg = float(weight_pounds) * 0.45
+# print('weight in pounds is ' + weight_pounds + ', weight in kg is ' + str(int(weight_kg)))
+
+# # Strings
+# course = '''Python's for "Beginners"'''
+# course2 = '''
+# Hi John,
+#
+# Here is our first email to you
+#
+# Thank you,
+# Support Team
+#
+# '''
+# course3 = 'Python for beniggners'
+# another = course3[:]
+# print(another)
+#
+# formatting String
+first = 'John'
+last = 'Smith'
+
+# John [Smith] is a coder
+
+message = first +' [' + last + ' ] is a coder'
+print(message)
+
