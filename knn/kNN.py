40e073567c6fa98bf6c700deb71e9a6cfe00546d
Jingyi3 <oliz0991@gmail.com>
2020-02-27 17:23:14 -0500

    some update

---
+++ knn/kNN.py
@@ -0,0 +1,22 @@
+import numpy as np
+from math import sqrt
+from collections import Counter
+
+def kNN_classify(k, X_train, y_train, x):
+
+    assert 1 <= k <= X_train.shape[0], "k must be valid"
+    assert X_train.shape[0] == y_train.shape[0], \
+        "the size of X_train must equal to the size of y_tain"
+    assert X_train.shape[1] == x.shape[0], \
+        "the feature number of x must be equal to X_train"
+
+    distances = [sqrt(np.sum(x_tarin - x)**2) for x_tarin in X_train]
+    nearest = np.argsort(distances)
+
+    topK_y = [y_train[i] for i in nearest[:k]]
+    votes = Counter(topK_y)
+
+    return votes.most_common(1)[0][0]
+
+
+
